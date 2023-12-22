67c7a32d1ef20c84063323b683bb847733251b92
lawsonroberts <111693722+lawsonroberts@users.noreply.github.com>
2023-12-22 17:29:29 -0600

    app refresh

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # nfl_statistics
-I want to do analysis on NFL game statistics to understand if game stats have any predicting power towards which team wins the game. 
+
+I want to do analysis on NFL game statistics to understand if game stats have any predicting power towards which team wins the game.
+
+## Table of Contents
+
+- [Environment](#environment)
+- [Project Structure](#project-structure)
+
+## Environment
+
+- [Python Environment](https://www.python.org/downloads/release/python-3111/)
+- [Requirements File](requirements.txt)
+
+  - in your python environment simply run `pip install -r requirements.txt` after cloning the repo in your command line
+
+- [Streamlit App = Hosted on public streamlit community](https://lawson-roberts-nfl-statistics-app-20jqx8.streamlit.app/)
+- if you would like to run locally to develop and make changes of your own just run the following after cloning the repo in your command line `streamlit run app.py`
+
+## Project Structure
+
+- Folders
+  - [.gitub](.github/) - coming soon. This is where all the Github CI yaml files / config lives.
+  - [CI Jobs](ci_jobs/) - coming soon. This is where all the Github CI jobs that will be getting executed.
+  - [data](data/) - includes raw data as well as data that is ready for the model to train on and predict.
+  - [models](models/) - There were a variety of models that I have tried, all with similar performance. All the pickled files live here.
+    - Currently running this random forest model [models](models/random_forest_model_1.sav) using paramters shown here
+    - `python rf_clf =RandomForestClassifier(max_depth = 10,n_estimators=40, max_features = 'log2', random_state=0)`
+  - [predictions](predictions/) - Holding model prediction files. These are used to score how the model is performing in production.
+- Files
+  - [Cleaning](cleaning_nfl_dataset.ipynb) - Exploring how I could clean this data and make transformation to be machine learning ready.
+  - [Classification Explore](nfl_dataset_exploration_classification.ipynb) - this is where I started. Later I realized I wouldn't have all the same features so pivoted to the notebook below `prediction_pipeline_exploration.ipynb`.
+  - [Prediction Pipeline Testing](prediction_pipeline_exploration.ipynb) - This was exploration on the data I would have avaialble at the time of prediction.
+  - [Pipeline](pipeline_in_order.ipynb) - This was exploration on what steps I needed for the CI jobs I will be working on next to automate training and collecting new data.
+  - [Data Collection - Web Scraping](scrap_nfl_game_stats_explore.ipynb) - This was exploration of how I could collect this data to train my model.
--- app.py
+++ app.py
@@ -18,6 +18,11 @@ import datetime
 import requests
 import pickle
 import io
+import matplotlib.pyplot as plt
+import shap
+from shap import Explanation
+shap.initjs()
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 ##Setting Streamlit Settings
 #st.set_page_config(layout="wide")
@@ -32,29 +37,46 @@ import io
 #historical = historical[historical['year'] == year]
 
 historical = pd.read_csv('data/agg_team_stats.csv')
-historical = historical.drop(columns='Unnamed: 0')
+# historical = historical.drop(columns='Unnamed: 0')
 
 ## declaring model name that I would like to use
 filename = 'models/random_forest_model_1.sav'
 # load the model from disk
 model_obj = pickle.load(open(filename, 'rb'))
 
+# df_ready_for_model_hist = pd.read_csv('data/df_ready_for_model.csv')
+# # df_ready_for_model_hist = df_ready_for_model_hist.drop(columns = 'Unnamed: 0')
+# df_model = df_ready_for_model_hist
+# df_model['target_class']  = np.where(df_model['target'] == 1, "away", "home")
+# print(df_model.groupby('target_class').size())
+# df_model = df_model.drop(columns = ['target'])
+# df_model = df_model.drop_duplicates()
+
+# data_bucket = df_model.copy()
+# data_bucket = data_bucket.dropna()
+# data_len = len(data_bucket.columns)
+
+# y_adj_bucket = data_bucket.iloc[:,data_len-1:]
+# X_adj_bucket = data_bucket.iloc[:,:data_len-1]
+
+
+
 ## loading predictions file to show accuracy
 predictions_hist = pd.read_csv('predictions/model_prediction_file.csv')
-predictions_hist = predictions_hist.drop(columns = 'Unnamed: 0')
+# predictions_hist = predictions_hist.drop(columns = 'Unnamed: 0')
 game_score_hist = pd.read_csv('data/game_scores.csv')
-game_score_hist = game_score_hist.drop(columns = 'Unnamed: 0')
+# game_score_hist = game_score_hist.drop(columns = 'Unnamed: 0')
 
-predictions_hist = predictions_hist.merge(game_score_hist, on='id')
-predictions_hist['tie_check'] = np.where(predictions_hist['away_score'] ==  predictions_hist['home_score'], 'tie', 'no tie')
-predictions_hist = predictions_hist[predictions_hist['tie_check'] == 'no tie']
-predictions_hist['winner'] = np.where(predictions_hist['away_score'] >  predictions_hist['home_score'], 'away', 'home')
-predictions_hist['prediction'] = np.where(predictions_hist['away'] >  predictions_hist['home'], 'away', 'home')
-predictions_hist['pred_correct'] = np.where(predictions_hist['winner'] == predictions_hist['prediction'], 1, 0)
-predictions_hist['away'] = round(predictions_hist['away']*100, 2)
-predictions_hist['home'] = round(predictions_hist['home']*100, 2)
+# predictions_hist = predictions_hist.merge(game_score_hist, on='id')
+# predictions_hist['tie_check'] = np.where(predictions_hist['away_score'] ==  predictions_hist['home_score'], 'tie', 'no tie')
+# predictions_hist = predictions_hist[predictions_hist['tie_check'] == 'no tie']
+# predictions_hist['winner'] = np.where(predictions_hist['away_score'] >  predictions_hist['home_score'], 'away', 'home')
+# predictions_hist['prediction'] = np.where(predictions_hist['away'] >  predictions_hist['home'], 'away', 'home')
+# predictions_hist['pred_correct'] = np.where(predictions_hist['winner'] == predictions_hist['prediction'], 1, 0)
+# predictions_hist['away'] = round(predictions_hist['away']*100, 2)
+# predictions_hist['home'] = round(predictions_hist['home']*100, 2)
 
-model_score = round((sum(predictions_hist['pred_correct']) / len(predictions_hist))*100, 2)
+# model_score = round((sum(predictions_hist['pred_correct']) / len(predictions_hist))*100, 2)
 
 st.title("NFL Game Predictions")
 st.write("""
@@ -64,10 +86,10 @@ st.write("""### Data Sources:""")
 st.write("""1.) https://www.espn.com/nfl/scoreboard/ used for getting game statistics""")
 st.write("""2.) Caesars Sportsbook used for vegas bets available.""")
 
-st.write("""### Model Results thus far...""")
-st.write("Accuracy Score", model_score, "%")
-st.write("""#### Game Detail for past predictions...""")
-st.write(predictions_hist.astype('object'))
+# st.write("""### Model Results thus far...""")
+# st.write("Accuracy Score", model_score, "%")
+# st.write("""#### Game Detail for past predictions...""")
+# st.write(predictions_hist.astype('object'))
 
 def get_current_games():
     url = "https://site.web.api.espn.com/apis/v2/scoreboard/header?sport=football&league=nfl&region=us&lang=en&contentorigin=espn&buyWindow=1m&showAirings=buy%2Clive%2Creplay&showZipLookup=true&tz=America/New_York"
@@ -97,8 +119,37 @@ game_df['time'] = game_df['time'].str.replace("18", "12")
 game_df['time'] = game_df['time'].str.replace("21", "03")
 game_df['time'] = game_df['time'].str.replace("01", "07")
 
-#col1, col2, col3 = st.beta_columns(3)
-col1, col2 = st.beta_columns(2)
+away_df = game_df[['away']].copy(deep=True)
+away_df = away_df.rename(columns = {'away': 'team'})
+away_df = away_df.merge(historical, on='team', how='inner')
+away_df = away_df.rename(columns = {'first_downs':'first_downs_away', 'first_downs_passing':'first_downs_passing_away', 'first_down_rushing':'first_down_rushing_away', 'first_down_by_penalty':'first_down_by_penalty_away', 'total_plays':'total_plays_away', 'total_yards':'total_yards_away', 'total_drives':'total_drives_away', 'yards_per_play':'yards_per_play_away', 'passing':'passing_away', 'yards_per_pass':'yards_per_pass_away', 'int_thrown':'int_thrown_away', 'rushing':'rushing_away', 'rushing_att':'rushing_att_away', 'yards_per_rush':'yards_per_rush_away', 'turnovers':'turnovers_away', 'fumbles_lost':'fumbles_lost_away', 'defensive_td':'defensive_td_away', 'possession':'possession_away', 'sack_yards_lost_occur':'sack_yards_lost_away_occur', 'sack_yards_lost_yards':'sack_yards_lost_away_yards', 'penalty_occur':'penalty_away_occur', 'penalty_yards':'penalty_away_yards'})
+away_df = away_df.reset_index(drop=True)
+away_df = away_df.drop(columns = ['team'])
+
+home_df = game_df[['home']].copy(deep=True)
+home_df = home_df.rename(columns = {'home': 'team'})
+home_df = home_df.merge(historical, on='team', how='inner')
+home_df = home_df.rename(columns = {'first_downs':'first_downs_home', 'first_downs_passing':'first_downs_passing_home', 'first_down_rushing':'first_down_rushing_home', 'first_down_by_penalty':'first_down_by_penalty_home', 'total_plays':'total_plays_home', 'total_yards':'total_yards_home', 'total_drives':'total_drives_home', 'yards_per_play':'yards_per_play_home', 'passing':'passing_home', 'yards_per_pass':'yards_per_pass_home', 'int_thrown':'int_thrown_home', 'rushing':'rushing_home', 'rushing_att':'rushing_att_home', 'yards_per_rush':'yards_per_rush_home', 'turnovers':'turnovers_home', 'fumbles_lost':'fumbles_lost_home', 'defensive_td':'defensive_td_home', 'possession':'possession_home', 'sack_yards_lost_occur':'sack_yards_lost_home_occur', 'sack_yards_lost_yards':'sack_yards_lost_home_yards', 'penalty_occur':'penalty_home_occur', 'penalty_yards':'penalty_home_yards'})
+home_df = home_df.reset_index(drop=True)
+home_df = home_df.drop(columns = ['team'])
+
+stats_all = pd.concat([away_df, home_df], axis=1)
+column_order = ['first_downs_away', 'first_downs_passing_away', 'first_down_rushing_away', 'first_down_by_penalty_away', 'total_plays_away', 'total_yards_away', 'total_drives_away', 'yards_per_play_away', 'passing_away', 'yards_per_pass_away', 'int_thrown_away', 'rushing_away', 'rushing_att_away', 'yards_per_rush_away', 'turnovers_away', 'fumbles_lost_away', 'defensive_td_away', 'possession_away', 'first_downs_home', 'first_downs_passing_home', 'first_down_rushing_home', 'first_down_by_penalty_home', 'total_plays_home', 'total_yards_home', 'total_drives_home', 'yards_per_play_home', 'passing_home', 'yards_per_pass_home', 'int_thrown_home', 'rushing_home', 'rushing_att_home', 'yards_per_rush_home', 'turnovers_home', 'fumbles_lost_home', 'defensive_td_home', 'possession_home', 'sack_yards_lost_away_occur', 'sack_yards_lost_away_yards', 'sack_yards_lost_home_occur', 'sack_yards_lost_home_yards', 'penalty_away_occur', 'penalty_away_yards', 'penalty_home_occur', 'penalty_home_yards']
+
+# order columns to match model
+stats_all = stats_all[column_order]
+prediction = model_obj.predict(stats_all)
+probabilities = pd.DataFrame(model_obj.predict_proba(stats_all), columns=['away', 'home'])
+# probabilities = probabilities.rename(columns = {'away': away, 'home': home})
+
+# Create an explainer
+explainer = shap.Explainer(model_obj)
+# Get feature importance values
+shap_values = explainer.shap_values(stats_all)
+shap_df = pd.DataFrame(shap_values[0])
+
+# col1, col2, col3 = st.columns(3)
+col1, col2 = st.columns(2)
 
 for ind in game_df.index:
 
@@ -134,6 +185,9 @@ for ind in game_df.index:
 
     stats_all = pd.concat([away_stat, home_stat], axis=1)
 
+    column_order = ['first_downs_away', 'first_downs_passing_away', 'first_down_rushing_away', 'first_down_by_penalty_away', 'total_plays_away', 'total_yards_away', 'total_drives_away', 'yards_per_play_away', 'passing_away', 'yards_per_pass_away', 'int_thrown_away', 'rushing_away', 'rushing_att_away', 'yards_per_rush_away', 'turnovers_away', 'fumbles_lost_away', 'defensive_td_away', 'possession_away', 'first_downs_home', 'first_downs_passing_home', 'first_down_rushing_home', 'first_down_by_penalty_home', 'total_plays_home', 'total_yards_home', 'total_drives_home', 'yards_per_play_home', 'passing_home', 'yards_per_pass_home', 'int_thrown_home', 'rushing_home', 'rushing_att_home', 'yards_per_rush_home', 'turnovers_home', 'fumbles_lost_home', 'defensive_td_home', 'possession_home', 'sack_yards_lost_away_occur', 'sack_yards_lost_away_yards', 'sack_yards_lost_home_occur', 'sack_yards_lost_home_yards', 'penalty_away_occur', 'penalty_away_yards', 'penalty_home_occur', 'penalty_home_yards']
+    # order columns to match model
+    stats_all = stats_all[column_order]
     prediction = model_obj.predict(stats_all)
     probabilities = pd.DataFrame(model_obj.predict_proba(stats_all), columns=['away', 'home'])
     probabilities = probabilities.rename(columns = {'away': away, 'home': home})
@@ -144,6 +198,14 @@ for ind in game_df.index:
         st.write("Home Team:", game_df['home'][ind])
         st.write("Home Team Money Line:", game_df['odds.home.moneyLine'][ind])
         st.write("Model Predicts...", round(probabilities[home][0]*100, 2), "% chance of winning")
+
+        # create Shap waterfall plot for home team using shap_values[0][ind] for shap values
+        # st.write("SHAP Values for Home Team Probability...")
+        # shap.plots._waterfall.waterfall_legacy(explainer.expected_value[0], shap_values[0][ind], stats_all.iloc[ind, :])
+        st.pyplot(shap.plots._waterfall.waterfall_legacy(explainer.expected_value[1], shap_values[1][ind], stats_all.iloc[0, :]), )
+        st.write("                         ")
+        st.write("                         ")
+
         st.write("-------------------------")
 
     with col2:
@@ -151,9 +213,23 @@ for ind in game_df.index:
         st.write("Away Team:", game_df['away'][ind])
         st.write("Away Team Money Line:", game_df['odds.away.moneyLine'][ind])
         st.write("Model Predicts...", round(probabilities[away][0]*100, 2), "% chance of winning")
-        #st.write("")
-        #t.write("")
+
+        st.pyplot(shap.plots._waterfall.waterfall_legacy(explainer.expected_value[0], shap_values[0][ind], stats_all.iloc[0, :]), )
+        st.write("")
         st.write("-------------------------")
+
+    # # create Shap waterfall plot for home team using shap_values[0][ind] for shap values
+    # st.write("SHAP Values for Home Team Probability...")
+    # # shap.plots._waterfall.waterfall_legacy(explainer.expected_value[0], shap_values[0][ind], stats_all.iloc[ind, :])
+    # st.pyplot(shap.plots._waterfall.waterfall_legacy(explainer.expected_value[0], shap_values[0][ind], stats_all.iloc[0, :]))
+    
+    # with col3:
+    #     st.write("")
+    #     # create Shap waterfall plot for home team using shap_values[0][ind] for shap values
+    #     # st.write("SHAP Values for Home Team Probability...")
+    #     # shap.plots._waterfall.waterfall_legacy(explainer.expected_value[0], shap_values[0][ind], stats_all.iloc[ind, :])
+    #     st.pyplot(shap.plots._waterfall.waterfall_legacy(explainer.expected_value[0], shap_values[0][ind], stats_all.iloc[0, :]), )
+    #     st.write("-------------------------")
 
     #with col3:
         #st.write(stats_all.astype('object'))
